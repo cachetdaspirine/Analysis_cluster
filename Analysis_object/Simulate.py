ad399dcd3279aba8b131abe596a20c2c47bb19f6
cachetdaspirine <leroyhugo@yahoo.fr>
2023-04-24 13:23:20 +0200

    create a module for multiprocessing gillespies system

--- Analysis_object/Simulate.py
+++ Analysis_object/Simulate.py
@@ -1,38 +1,77 @@
 import numpy as np
+from multiprocessing import Pool
 import sys
-sys.path.append('/home/hcleroy/PostDoc/aging_condensates/Simulation/Gillespie/Gillespie_backend')
+sys.path.append('/home/hcleroy/PostDoc/aging_condensates/Simulation/Aging_Condensate/Gillespie_backend')
 import Gillespie_backend as Gil
 
 class ParallelSimulation:
     """
     This class is used to quickly simulate a system, extract some parameters on several nodes.
-    
-    output :    is a dictionnary, to obtain the list of possible outputs call possible_outputs. the last
-                argument is a list that tells if each of the previous output should be averaged or not.
+    To make a simulation, one need to edit a child class and write the extract_parameter function
+
+    gillespie_param is a dictionnary. One or several variable is an array, a list of parameters compatible with the 
+    use of a starmap is then created.
                 
     """
     def __init__(self,
                  gillespie_param,
                  simulation_param,
-                 output,
                  cpu_param):
         self.gillespie_param = gillespie_param
         self.step_tot = simulation_param['step_tot']
-       
+        self.dump_step = simulation_param['dump_step']
         self.Nnodes = cpu_param['Nnodes']
+        self.gillespieParamNames = ['ell_tot','rho0','BindingEnergy','kdiff','seed','sliding','Nlinker','old_gillespie','dimension']
+        self.default_values = {'ell_tot':100,'rho0':0.1,'BindingEnergy':-1.,'kdiff':1.,'seed':19874,'sliding':False,'Nlinker':0,'old_gillespie':None,'dimension':3}
+        self.make_args()
+    def make_args(self):
+        # first find which key has an array as a value:
+        ValueLength=0
+        self.keyarray = ''
+        for key,value in self.gillespie_param.items():
+            if type(value)==np.ndarray or type(value)==list :
+                ValueLength = value.__len__()
+                self.keyarray = key
+        self.args = []
+        k = 0
+        for key in self.gillespieParamNames:
+            if key==self.keyarray:
+                self.index_of_keyarray = k
+            else:
+                k+=1
+            try:
+                value = self.gillespie_param[key]
+            except KeyError:
+                value = self.default_values[key]
+            if type(value)!=np.ndarray and type(value)!=list:
+                self.args.append([value for _ in range(ValueLength)])
+            else :
+                self.args.append(value)
+        self.args = np.transpose(self.args)
+        self.args = [tuple(row) for row in self.args]
+    def Run_One_system(self,a1,a2,a3,a4,a5,a6,a7,a8,a9):
+        arg = [a1,a2,a3,a4,a5,a6,a7,a8,a9]
+        res = dict()
+        gillespie  = Gil.Gillespie(a1,a2,a3,a4,a5,a6,a7,a8,a9)
+        for step in range(self.step_tot//self.dump_step):
+            move,time = gillespie.evolve(self.dump_step)
+            res.update(self.extract_parameter(gillespie,move,time,(step+1)*self.dump_step,name = str(arg[self.index_of_keyarray])))
+        return res
 
-        self.output = output
-    def possible_outputs(self):
-        print('R, r, ell average')
-        return {'R':False,'r':False,'ell':False,'average':[False,False,False]}
-    def actualize_outputs(self,output):
-        self.output = output
-    
-    def extract_parameter(self,gillespie):
+    def Parallel_Run(self):
+        pool = Pool(self.Nnodes)
+        res = pool.starmap(self.Run_One_system,self.args)
+        return self.unpack_res(res)
+
+    def unpack_res(self,res):
         """
-        given a dictionnary of outputs, and a gillespie object, this function
-        extract the relevant parameters
+        This function unpack the results and has to be edited in the child function
         """
-        for key,value in self.output:
-            if key == 'R':
-                
+        return
+    def extract_parameter(self,gillespie,moves,time,name):
+        """
+        This function has to be edited in the child class
+        name is a way to recognize from which gillespie object the parameters are being extracted.
+        """
+        return
+         
